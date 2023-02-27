d7144edda07f4ff4bf03ce66ce6ca8bbfadcc5d4
Семён Курочкин <sema.kurochkin@mail.ru>
2023-02-27 23:22:16 +0300

    Задача 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static Shares;
+
+int size = Input("Введите количество чисел");
+int[] array = InputArray(size);
+int pos = Positive(array);
+Console.WriteLine("Количество положительных элементов = " + pos);
